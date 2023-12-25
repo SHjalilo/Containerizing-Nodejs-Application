754ee555c63158525acd9056ccc1521dd43e04bf
SHjalilo <aaaabdjalil@gmail.com>
2023-12-26 00:46:56 +0100

    first coommit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const redis = require('redis');
+
+const app = express();
+
+const client = redis.createClient({
+    host : 'redis',
+    port : 6379
+});
+
+client.set('visitsCounter', 0);
+
+app.get('/', (req,res) =>{
+    client.get('visitsCounter',(err,visitsCounter)=>{
+        //
+    res.send('visitsCounter : '+ visitsCounter);
+        //
+    client.set('visitsCounter', parseInt(visitsCounter) +1);
+
+    })
+});
+
+app.listen(8000, ()=>{
+    console.log('listening on port 8000');
+});
